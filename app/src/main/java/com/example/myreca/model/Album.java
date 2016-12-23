33a7a24f9ecef7115e1eb7a470507a64823ebf14
mrMgt <linhdinhnguyen@gmail.com>
2016-12-23 17:18:17 +0700

    add new MyReCaApplication

---
+++ app/src/main/java/com/example/myreca/model/Album.java
@@ -0,0 +1,44 @@
+package com.example.myreca.model;
+
+/**
+ * Created by linhnd on 2016/12/23.
+ */
+
+public class Album {
+    private String name;
+    private int numOfSongs;
+    private int thumbnail;
+
+    public Album() {
+    }
+
+    public Album(String name, int numOfSongs, int thumbnail) {
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
